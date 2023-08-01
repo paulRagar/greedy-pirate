2ccc3d82fb51a11bd0203804d45f4a07729c57b3
Paul Ragar <paul.ragar@gmail.com>
2023-07-31 20:58:31 -0600

    Removes fetch request in home page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,12 @@
 import HomeClient from './HomeClient';
 
 const Home = async () => {
-   const result = await fetch(`${process.env.BASE_URL}/api/hello`, {
-      method: 'GET',
-      headers: { 'Content-type': 'application/json' },
-   }).then((res) => res.json());
+   // const result = await fetch(`${process.env.BASE_URL}/api/hello`, {
+   //    method: 'GET',
+   //    headers: { 'Content-type': 'application/json' },
+   // }).then((res) => res.json());
 
-   console.log('result:', result);
+   // console.log('result:', result);
 
    return <HomeClient numberOfPlayers={2} />;
 };
