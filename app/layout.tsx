ea1b7e66288f89094c17f69cc093d9c4943ed4e8
Paul Ragar <paul.ragar@gmail.com>
2023-08-23 15:29:14 -0600

    Adds some SEO tests

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <html lang='en'>
          <head>
             <title>Greedy Pirate</title>
+            <meta
+               name='description'
+               content='Embark on a thrilling voyage with Greedy Pirate, where a treasure of coins awaits, but beware - one wrong card could plunder your loot in this high-stakes, risk-and-reward browser card game!'
+            />
+            <meta
+               name='keywords'
+               content='greedy, pirate,Greedy Pirate, Card game, Pirate card game, Treasure collecting game, Risk-reward game, Strategy card game, Coin collecting game, Draw and bank game, Pirate-themed game, Family game, Board game night, Fun card games, Card game with coins, Card banking game, Avoid the pirate card, Treasure card game, Multiplayer card game, Tabletop pirate game, Greedy Pirate rules, Card game strategies, Pirate treasure hunt game, Game of luck and strategy, Streak based card game, Family game night ideas, Games like Greedy Pirate, Card draw strategy, Pirate coin game, Bank or bust game, Easy to learn card games, Competitive card games'
+            />
+            <meta property='og:title' content='Greedy Pirate' />
+            <meta
+               property='og:description'
+               content='Embark on a thrilling voyage with Greedy Pirate, where a treasure of coins awaits, but beware - one wrong card could plunder your loot in this high-stakes, risk-and-reward browser card game!'
+            />
+            <meta property='og:url' content='https://greedypirate.com' />
+            <link rel='canonical' href='https://greedypirate.com' />
          </head>
          <body>
             <div className='h-screen w-screen max-w-[1460px] m-auto p-4 bg-gray-200 dark:bg-slate-900'>{children}</div>
